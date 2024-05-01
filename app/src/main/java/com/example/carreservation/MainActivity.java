fab4c0ea0508d030c56e42b18726007da404efd3
Muhammad Bilal <mbilal254.m@gmail.com>
2024-05-01 21:20:16 +0500

    add firebase push notification

--- app/src/main/java/com/example/carreservation/MainActivity.java
+++ app/src/main/java/com/example/carreservation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.carreservation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,72 +12,96 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.Source;
-import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final long SPLASH_DELAY = 2000;
-
-    public void onTokenRefresh() {
-        // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d("TAG", "Refreshed token: " + refreshedToken);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-//        sendRegistrationToServer(refreshedToken);
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        onTokenRefresh();
+        // Notification System
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            System.out.println("Fetching FCM registration token failed" + task.getException());
+                            return;
+                        }
 
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                        // Get new FCM registration token
+                        String token = task.getResult();
+//                        getToken = token;
 
-        if (mAuth.getCurrentUser() != null) {
+//                        System.out.println("Device Token : "+ getToken);
 
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            db.collection("Users")
-                    .document(mAuth.getCurrentUser().getUid()).get().addOnSuccessListener(
-                            new OnSuccessListener<DocumentSnapshot>() {
+                        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+                        if (mAuth.getCurrentUser() != null) {
+
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                            System.out.println("Token " + token.isEmpty());
+                            db.collection("Users").document(mAuth.getCurrentUser().getUid()).update("FCM_Token", token).addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
-                                public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                                    System.out.println("++++++++++++++++++++++++");
-//                                    System.out.println(documentSnapshot.getData());
-                                    String userType = documentSnapshot.getData().get("userType").toString();
-                                    if (userType.equalsIgnoreCase("Customer")) {
-                                        Toast.makeText(MainActivity.this, "Customer", Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(MainActivity.this, UserDashboardActivity.class);
-                                        startActivity(intent);
-                                        finish();
-                                    }else{
-                                        Toast.makeText(MainActivity.this, "Vendor", Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(MainActivity.this, VendorDrawerActivity.class);
-                                        startActivity(intent);
-                                        finish();
-                                    }
+                                public void onSuccess(Void unused) {
+                                    System.out.println("Token Updated" + token);
+                                    db.collection("Users")
+                                            .document(mAuth.getCurrentUser().getUid()).get().addOnSuccessListener(
+                                                    new OnSuccessListener<DocumentSnapshot>() {
+                                                        @Override
+                                                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                                            System.out.println("++++++++++++++ UID ++++++++++");
+                                                            System.out.println(documentSnapshot.getData().get("uuid"));
+                                                            String userType = documentSnapshot.getData().get("userType").toString();
+                                                            if (userType.equalsIgnoreCase("Customer")) {
+                                                                Toast.makeText(MainActivity.this, "Customer", Toast.LENGTH_SHORT).show();
+                                                                Intent intent = new Intent(MainActivity.this, UserDashboardActivity.class);
+                                                                startActivity(intent);
+                                                                finish();
+                                                            } else {
+                                                                Toast.makeText(MainActivity.this, "Vendor", Toast.LENGTH_SHORT).show();
+                                                                Intent intent = new Intent(MainActivity.this, VendorDrawerActivity.class);
+                                                                startActivity(intent);
+                                                                finish();
+                                                            }
+                                                        }
+                                                    }
+                                            );
                                 }
-                            }
-                    );
-        } else {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent intent = new Intent(MainActivity.this, CommonLoginActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }, SPLASH_DELAY);
-        }
+                            });
+
+
+                        } else {
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(MainActivity.this, CommonLoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }, SPLASH_DELAY);
+                        }
+
+
+                    }
+                });
+
+//         f8lejZvuSqi8YY9YrvGohx:APA91bFbQn23YT91r9SY9QjMABXQVgkMeP6lLyEerObrfEmOyVAXI_CkSj-td9sz9jK3VhXi3xvnPHLUSeplAr1
+
+
     }
 }
